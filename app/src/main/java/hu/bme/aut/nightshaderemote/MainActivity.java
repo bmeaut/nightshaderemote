9975638fa1f75c40ed8bc73b0ad3c57b938e8199
Ákos Pap <papkos@gmail.com>
2014-02-14 19:11:21 +0100

    minVersion: 10 (Gingerbread - 2.3.3) AppCompat library, ActionBarActivity

--- app/src/main/java/hu/bme/aut/nightshaderemote/MainActivity.java
+++ app/src/main/java/hu/bme/aut/nightshaderemote/MainActivity.java
@@ -1,12 +1,12 @@
-package hu.bme.aut.elteplanetarium;
+package hu.bme.aut.nightshaderemote;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 
 /**
  * Created by akos on 2/14/14.
  */
-public class MainActivity extends Activity {
+public class MainActivity extends ActionBarActivity {
 
     public static final String TAG = "MainActivity";
 
