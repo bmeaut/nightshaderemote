caf717dc69b701cdcac88a13e65f32ba6f0d6bd5
Ákos Pap <papkos@gmail.com>
2014-03-06 16:21:44 +0100

    basic example to demonstrate functionality

--- app/src/main/java/hu/bme/aut/nightshaderemote/MainActivity.java
+++ app/src/main/java/hu/bme/aut/nightshaderemote/MainActivity.java
@@ -2,11 +2,15 @@ package hu.bme.aut.nightshaderemote;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.Toast;
+
+import hu.bme.aut.nightshaderemote.connectivity.SendCommand;
 
 /**
  * Created by akos on 2/14/14.
  */
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements SendCommand.OnCommandSentListener {
 
     public static final String TAG = "MainActivity";
 
@@ -14,5 +18,19 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.tgbtnConstLines).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String command = "/constellationLines/toggle";
+
+                new SendCommand("192.168.0.108", 8888, command, MainActivity.this).execute();
+            }
+        });
+    }
+
+    @Override
+    public void onCommandSent(String result) {
+        Toast.makeText(this, result, Toast.LENGTH_SHORT).show();
     }
 }
---
+++ app/src/main/java/hu/bme/aut/nightshaderemote/connectivity/SendCommand.java
@@ -0,0 +1,80 @@
+package hu.bme.aut.nightshaderemote.connectivity;
+
+import android.os.AsyncTask;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
+
+/**
+ * Created by akos on 3/6/14.
+ */
+public class SendCommand extends AsyncTask<Void, Void, String> {
+    public static final String TAG = "SendCommand";
+
+    protected String ip = "192.168.0.108";
+    protected int port = 8888;
+    protected String uri;
+
+    final OnCommandSentListener callback;
+
+    public SendCommand(String ip, int port, String uri, OnCommandSentListener callback) {
+        this.ip = ip;
+        this.port = port;
+        this.uri = uri;
+        this.callback = callback;
+    }
+
+    @Override
+    protected String doInBackground(Void... params) {
+        URL url = null;
+        HttpURLConnection urlConnection = null;
+
+        String result = "--- no result ---";
+        try {
+            url = new URL("http://" + ip + ":" + port + uri);
+            urlConnection = (HttpURLConnection)url.openConnection();
+
+            try {
+                InputStream in = urlConnection.getInputStream();
+                result = readStream(in);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (urlConnection != null) urlConnection.disconnect();
+        }
+
+        return result;
+    }
+
+    @Override
+    protected void onPostExecute(String s) {
+        if (callback != null) {
+            callback.onCommandSent(s);
+        }
+    }
+
+    /**
+     * Beolvassa a Stream tartalmát egy stringbe.
+     * @param inputStream Ahonnan olvas.
+     * @return Amit beolvasott. Üres, ha nem jött semmi.
+     * @throws java.io.IOException Ha hiba történt a streamből olvasáskor.
+     */
+    public static String readStream(InputStream inputStream) throws IOException {
+        // http://stackoverflow.com/questions/309424/read-convert-an-inputstream-to-a-string
+        Scanner scanner = new Scanner(inputStream).useDelimiter("\\A");
+        return (scanner.hasNext()) ? scanner.next() : "";
+    }
+
+    public interface OnCommandSentListener {
+        void onCommandSent(String result);
+    }
+}
