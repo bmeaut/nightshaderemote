2dbb18c22e004111df38c51ce49dce1a1d8f2193
Ákos Pap <papkos@gmail.com>
2014-04-29 19:38:47 +0200

    Marci változtatásai: Négyzet alakú FlagButton-ok NewCustomButton dialog: scrollozható

---
+++ app/src/main/java/hu/bme/aut/nightshaderemote/ui/SquareToggleButton.java
@@ -0,0 +1,37 @@
+package hu.bme.aut.nightshaderemote.ui;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ToggleButton;
+
+/**
+ * Created by Marci on 2014.04.29..
+ */
+public class SquareToggleButton extends ToggleButton {
+    public SquareToggleButton(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    public SquareToggleButton(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public SquareToggleButton(Context context) {
+        super(context);
+    }
+
+    @Override
+      protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        // Négyzetes felület kikényszerítése
+        int w = MeasureSpec.getSize(widthMeasureSpec);
+        int h = MeasureSpec.getSize(heightMeasureSpec);
+        int d = w == 0
+                ? h
+                : h == 0
+                ? w
+                : w < h
+                ? w
+                : h;
+        setMeasuredDimension(d, d);
+    }
+}
